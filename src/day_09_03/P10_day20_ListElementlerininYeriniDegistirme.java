5b59cf394422bcace072c3556cf3070ca8b33791
unknown <ali8184@yahoo.com>
2023-03-09 11:24:13 +0300

    09_03_Bireysel Çalışma

---
+++ src/day_09_03/P10_day20_ListElementlerininYeriniDegistirme.java
@@ -0,0 +1,31 @@
+package day_09_03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P10_day20_ListElementlerininYeriniDegistirme {
+    public static void main(String[] args) {
+
+
+        List<Integer> sayilar=new ArrayList<>();
+
+        sayilar.add(3);
+        sayilar.add(5);
+        sayilar.add(1);
+        sayilar.add(4);
+        sayilar.add(3);
+        sayilar.add(7);
+        sayilar.add(10);
+
+        int temp=0;
+
+        temp=sayilar.get(2);
+        System.out.println(sayilar.set(2, sayilar.get(5)));
+        sayilar.set(5,temp);
+        System.out.println(sayilar);
+
+    }
+
+
+
+}
